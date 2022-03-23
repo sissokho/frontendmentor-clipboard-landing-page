5af6340439a045065e2c4a8b029b8276b0eed43d
Mouhamadou Moustapha SISSOKHO <siskomouhamed@gmail.com>
2022-03-23 16:47:01 +0000

    Integrate style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
 module.exports = {
     content: ['./index.html'],
     theme: {
+        colors: {
+            cyan: 'hsl(171, 66%, 44%)',
+            white: 'hsl(0, 0%, 100%)',
+            blue: {
+                light: 'hsl(233, 100%, 69%)',
+                grayish: 'hsl(201, 11%, 66%)',
+                darkgrayish: 'hsl(210, 10%, 33%)',
+            },
+            fontFamily: {
+                'bai-jamjuree': ['Bai Jamjuree', 'sans-serif'],
+            },
+            fontWeight: {
+                regular: '400',
+                semibold: '600',
+            },
+        },
         extend: {},
     },
     plugins: [],
